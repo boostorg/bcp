fd822c9b8c0d00a31ac0d2711c30c8805d4bbdde
jzmaddock <john@johnmaddock.co.uk>
2022-07-14 11:50:20 +0100

    Move inspect testing into new script.

--- test/ci_script.sh
+++ test/ci_script.sh
@@ -20,3 +20,15 @@ ls -l
 ./b2 -j3 libs/program_options/build  || { echo './b2 -j3 libs/program_options/build failed' ; exit 1; }
 ./b2 -j3 --with-program_options || { echo './b2 -j3 --with-program_options failed' ; exit 1; }
 ./b2 -j3  || { echo './b2 -j3 failed' ; exit 1; }
+
+# run inspect:
+cd ../boost-root
+echo "using xsltproc ;" | tee -a $HOME/user-config.jam
+echo "using boostbook : /usr/share/xml/docbook/stylesheet/docbook-xsl : /usr/share/sgml/docbook/dtd/xml/4.2 ;" | tee -a $HOME/user-config.jam
+cd tools/inspect/build
+../../../b2 -j2 address-model=64 architecture=x86 toolset=gcc cxxflags="-std=gnu++14" release dist-bin || { echo 'building inspect failed' ; exit 1; }
+cd ../../bcp/doc 
+rm -rf html 
+../../../b2 -j2 address-model=64 architecture=x86 toolset=gcc cxxflags="-std=gnu++14" release || { echo 'building docs failed' ; exit 1; }
+../../../dist/bin/inspect -text || { echo 'inspect failed' ; exit 1; }
+
